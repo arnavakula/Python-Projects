33dff3718986f4ab5f53ff05c47c3e6354d6c082
Arnav Akula <akula.arnav@gmail.com>
2020-06-04 18:58:33 -0700

    Clean code -added reusable functions

--- textpro/text_pro.py
+++ textpro/text_pro.py
@@ -1,20 +1,26 @@
-
-
 statements = []
 questions = ['How', 'Why', 'What', 'Where', 'When', 'Which', 'Who', 'Whom']
+
+def capitalize(user_input):
+    words = user_input.split(' ')
+    words[0] = words[0].title()
+    return words
+
+def punctuate(words):
+    if(words[0] in questions): 
+        words[-1] += '?'
+    else:
+        words[-1] += '.'
+    return words
 
 while(True):
     user_input = input("Say something: ")
     if(user_input.lower() == '\end'): 
         break
     else:
-        words = user_input.split(' ')
-        words[0] = words[0].title()
-        if(words[0] in questions): 
-            words[-1] += '?'
-        else:
-            words[-1] += '.'
-        statements.append(' '.join(words))
+        words = capitalize(user_input)
+        statements.append(' '.join(punctuate(words)))
 
 for s in statements:
     print(s)
+
